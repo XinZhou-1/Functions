dc0424009988e2f067424e3270bf887d651e1d43
XinZhou-1 <xinxinchou90@gmail.com>
2024-09-27 11:27:31 +0800

    Add period for the climatology

--- nc_data_anom.py
+++ nc_data_anom.py
@@ -1,17 +1,17 @@
-def daily_anom(daily_var,clim=False):
-    #daily climatology 
-    var_clim = daily_var.groupby('time.dayofyear').mean('time').load()
-    #daymean anomalies
+def daily_anom(daily_var,ystr,yend,clim=False):
+    #daily climatology, from ystr to yend  
+    var_clim = daily_var.sel(time = slice(f'{ystr}-01-01',f'{yend}-12-31')).groupby('time.dayofyear').mean('time').load()
+    #daymean anomalies, relative to the climatology ystr-yend
     var_anom = daily_var.groupby('time.dayofyear') - var_clim
     if(clim==True):
         return var_anom, var_clim
     else:
         return var_anom
     
-def monthly_anom(mon_var,clim=False):
-    #monthly climatology 
-    var_clim = mon_var.groupby('time.month').mean('time').load()
-    #monthly anomalies
+def monthly_anom(mon_var,ystr,yend,clim=False):
+    #monthly climatology, from ystr to yend
+    var_clim = mon_var.sel(time = slice(f'{ystr}-01-01',f'{yend}-12-31')).groupby('time.month').mean('time').load()
+    #monthly anomalies, relative to the climatology ystr-yend
     var_anom = mon_var.groupby('time.month') - var_clim
     if(clim==True):
         return var_anom, var_clim
